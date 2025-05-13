df2407b72a38274a600c3a035f6e64dca3a93cda
kalyan10001 <venkatakalyan1000@gmail.com>
2025-05-13 15:41:11 +0530

    Clerk Setup Completed -- For User Authentication

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <SignedIn>
+        <SignOutButton/>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+    </div>
+  )
+}
+
+export default page
